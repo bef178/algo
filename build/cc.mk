0b58cd1caeceb9b5e47c4b806e1a00101a831316
tanghao <tanghao.hb@gmail.com>
2018-12-16 22:47:24 +0800

    refine cc.mk

--- build/cc.mk
+++ build/cc.mk
@@ -24,32 +24,31 @@ OBJECT_FILES := $(patsubst $(SRC)/%.c,$(OUT)/%.o,$(SOURCE_FILES))
 CC := gcc
 CCFLAGS = -std=c99 -Werror $(addprefix -include ,$(HEADER_FILES))
 
-.PHONY : dummy
-dummy :
+.PHONY: dummy
+dummy:
 
-.PHONY : out
-out : $(OBJECT_FILES)
-	@echo "linking ..."
+.PHONY: out
+out: $(OBJECT_FILES)
+	@echo "linking [$(TARGET_FILE)] ..."
 	@-mkdir -p $(shell dirname $(TARGET_FILE))
 	@$(CC) $(CCFLAGS) -o $(TARGET_FILE) $^
 
-.PHONY : a
-a : $(OBJECT_FILES)
-	@echo "archiving ..."
+.PHONY: a
+a: $(OBJECT_FILES)
+	@echo "archiving [$(TARGET_FILE)] ..."
 	@ar cr $(TARGET_FILE) $^
 
-.PHONY : o
-o : $(OBJECT_FILES)
-	@echo "linking ..."
+.PHONY: o
+o: $(OBJECT_FILES)
+	@echo "linking [$(TARGET_FILE)] ..."
 	@ld -r $^ -o $(TARGET_FILE)
 
-$(OUT)/%.o : $(SRC)/%.c
-	@echo "compiling ..."
+$(OUT)/%.o: $(SRC)/%.c $(HEADER_FILES)
+	@echo "compiling [$@] ..."
 	@-mkdir -p $(@D)
 	@$(CC) $(CCFLAGS) -c $< -o $@
 
 .PHONY: clean
 clean:
 	@echo "cleaning ..."
-	@rm -rf $(OUT_TARGET)
 	@rm -rf $(OUT)
