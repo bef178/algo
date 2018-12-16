89f42dc45da9c24af98879d1d93e7a12f7d73ba1
tanghao <tanghao.hb@gmail.com>
2018-12-16 22:51:26 +0800

    remove unused max() and min()

--- src/Util.c
+++ src/Util.c
@@ -15,11 +15,3 @@ void swap(int * a, int i, int j) {
     a[i] = a[j];
     a[j] = t;
 }
-
-int min(int i, int j) {
-    return i <= j ? i : j;
-}
-
-int max(int i, int j) {
-    return i >= j ? i : j;
-}
