41f106fa35b85dbfabb99420a857b9de9f76ac50
tanghao <tanghao.hb@gmail.com>
2018-12-20 00:05:45 +0800

    update ShellSort comment

--- src/sort/ShellSort.c
+++ src/sort/ShellSort.c
@@ -3,8 +3,7 @@
  * with inner insertion sort
  */
 void ShellSort_sort(int * a, int l, int r) {
-    // using Marcin Ciura's gap sequence
-    static const int gaps[] = { 701, 301, 132, 57, 23, 10, 4, 1 };
+    static const int gaps[] = { 701, 301, 132, 57, 23, 10, 4, 1 }; // Ciura, 2001
     for (int k = 0; k < sizeof(gaps) / sizeof(int); k++) {
         int gap = gaps[k];
         for (int i = l + gap; i < r; i++) {
