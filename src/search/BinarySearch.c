ff3a4d6a1f22e0b9b168d9bbbd45a4a9ba7815ef
tanghao <tanghao.hb@gmail.com>
2018-12-24 23:59:01 +0800

    update BinarySearch add ordinary "search" function

--- src/search/BinarySearch.c
+++ src/search/BinarySearch.c
@@ -1,7 +1,29 @@
+/**
+ * return the 1st index of @v
+ */
+int BinarySearch_search(int * a, int l, int r, int v) {
+    r--;
+    while (l <= r) {
+        int m = l + (r - l) / 2;
+        if (v == a[m]) {
+            // find the 1st
+            while (m > 0 && v == a[m - 1]) {
+                --m;
+            }
+            return m;
+        } else if (v < a[m]) {
+            r = m - 1;
+        } else {
+            l = m + 1;
+        }
+    }
+    return -1;
+}
+
 static int upperBound(int * a, int l, int r, int v) {
     while (l < r) {
         int m = (r - l) / 2 + l;
-        if (a[m] > v) {
+        if (v < a[m]) {
             r = m;
         } else {
             l = m + 1;
@@ -13,7 +35,7 @@ static int upperBound(int * a, int l, int r, int v) {
 static int lowerBound(int * a, int l, int r, int v) {
     while (l < r) {
         int m = (r - l) / 2 + l;
-        if (a[m] < v) {
+        if (v > a[m]) {
             l = m + 1;
         } else {
             r = m;
