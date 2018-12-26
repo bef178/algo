c96c1f0cc571eccc1fbeb416f7e29b67e775809c
tanghao <tanghao.hb@gmail.com>
2018-12-26 20:49:37 +0800

    add problem: checksum of id number

---
+++ src/problem/IdChecksum.c
@@ -0,0 +1,28 @@
+/**
+ * 身份证号校验
+ *
+ * index    0  1  2  3  4  5  6  7  8  9 10 11 12 13 14 15 16 17
+ * p        1  1  0  1  0  8  1  9  4  9  1  0  0  1  0  0  1  4
+ * p[17] is checksum
+ * i = 0..16
+ * k = (p[i] * pow(2, 17-i)) % 11
+ */
+
+int checksum(const char * p) {
+    int k = 0;
+    for (int i = 0; i < 17; i++) {
+        k += (p[i] - '0') * (1 << (17 - i));
+        k %= 11;
+    }
+    char c[11] = { '1', '0', 'x', '9', '8', '7', '6', '5', '4', '3', '2' };
+    return c[k];
+}
+
+#include <assert.h>
+#include <stdio.h>
+
+void testChecksum() {
+    char * p = "110108194910010014"; // 北京海淀
+    assert(p[17] == checksum(p));
+    printf("%s %c\n", p, checksum(p));
+}
