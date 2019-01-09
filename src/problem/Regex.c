9d2f9565500da71e8e9eb4f214dd8e6385dce8f8
tanghao <tanghao.hb@gmail.com>
2019-01-09 17:22:13 +0800

    add problem: regex match dp algo

--- src/problem/Regex.c
+++ src/problem/Regex.c
@@ -4,6 +4,8 @@
  */
 
 #include <assert.h>
+#include <stdlib.h>
+#include <string.h>
 
 static boolean matchesRegex(const byte * s, const byte * regex) {
     assert(s != NULL);
@@ -64,6 +66,73 @@ static boolean matchesRegex(const byte * s, const byte * regex) {
     return false;
 }
 
+static boolean matchesRegex_dp(const byte * s, const byte * regex) {
+    assert(s != NULL);
+    assert(regex != NULL);
+
+    int n = strlen(regex);
+
+    // a valid regex will not start with '*' and have no neighboring '*'
+    assert(regex[0] != '*');
+    for (int i = 1; i < n; i++) {
+        if (regex[i] == '*') {
+            assert(regex[i - 1] != '*');
+        }
+    }
+
+    // imagine @regex in a horizontal line, while @s in vertical
+    // for regex[0..n) and s[0..m), the matrix should be m[m + 1][n + 1], extra "1" for empty string
+    // that is, for string s[0..j] and regex[0..i], the matching result saves to m[j + 1][i + 1]
+    // and, updating a row in matrix only requires the above row, so keeping tow rows to save memory
+
+    boolean * p = calloc(n + 1, sizeof(boolean));
+
+    // init current matching row
+    p[0] = true; // empty string matches empty string
+    for (int i = 0; i < n; i++) {
+        if (regex[i] == '*') {
+            p[i + 1] = p[i - 2 + 1];
+        } else {
+            p[i + 1] = false;
+        }
+    }
+
+    boolean * q = calloc(n + 1, sizeof(boolean));
+    for (int j = 0; s[j] != NUL; j++) {
+        // swap
+        boolean * t = q;
+        q = p;
+        p = t;
+        p[0] = false; // non-empty s does not match empty regex
+        for (int i = 0; i < n; i++) {
+            if (regex[i] == '*') {
+                if (regex[i - 1] == '.' || regex[i - 1] == s[j]) {
+                    p[i + 1] = q[i + 1] // a* as many a
+                        || p[i - 1 + 1] // a* as single a
+                        || p[i - 2 + 1]; // a* as empty
+                } else {
+                    // a* as empty
+                    p[i + 1] = p[i - 2 + 1];
+                }
+            } else if (regex[i] == '.') {
+                p[i + 1] = q[i - 1 + 1];
+            } else {
+                if (regex[i] == s[j]) {
+                    p[i + 1] = q[i - 1 + 1];
+                } else {
+                    p[i + 1] = false;
+                }
+            }
+        }
+    }
+
+    boolean matches = p[n];
+    free(p);
+    free(q);
+
+    return matches;
+}
+
 boolean Regex_matches(const byte * regex, const byte * s) {
-    return matchesRegex(s, regex);
+    return matchesRegex_dp(s, regex);
 }
