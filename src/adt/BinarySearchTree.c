ccf1a6b3f5216c8dffa944e7b3f5341bcdabb338
tanghao <tanghao.hb@gmail.com>
2020-06-17 19:03:21 +0800

    add BinarySearchTree method

--- src/adt/BinarySearchTree.c
+++ src/adt/BinarySearchTree.c
@@ -16,3 +16,46 @@ void BinarySearchTree_add(BinaryTreeNode * root, int64 value) {
         }
     }
 }
+
+/**
+ * return the root
+ */
+void BinarySearchTree_add2(BinaryTreeNode * root, int64 value) {
+    assert(root != NULL);
+    BinaryTreeNode * p = root;
+    while (true) {
+        if (value < p->value) {
+            if (p->l == NULL) {
+                p->l = BinaryTreeNode_malloc(value);
+                break;
+            } else {
+                p = p->r;
+            }
+        } else {
+            if (p->r == NULL) {
+                p->r = BinaryTreeNode_malloc(value);
+                break;
+            } else {
+                p = p->r;
+            }
+        }
+    }
+}
+
+/**
+ *      u               w
+ *     / \             / \
+ *    w   c     =>    a   u
+ *   / \                 / \
+ *  a   b               b   c
+ *
+ * return the new root; pending join parent and new root
+ */
+BinaryTreeNode * BinarySearchTree_rotateR(BinaryTreeNode * u) {
+    assert(u != NULL);
+    BinaryTreeNode * w = u->l;
+    assert(w != NULL);
+    u->l = w->r;
+    w->r = u;
+    return w;
+}
