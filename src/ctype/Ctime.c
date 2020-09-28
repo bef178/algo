27277829f545f86a4f138829609cc69458f06eba
tanghao <tanghao.hb@gmail.com>
2020-09-28 17:52:04 +0800

    rename time/TimeUtil => ctype/Ctime

--- src/ctype/Ctime.c
+++ src/ctype/Ctime.c
@@ -1,4 +1,102 @@
 /**
+ * Date and Time on Gregorian calendar
+ *
+ * Age of the universe: 1.38e10y
+ * 1d = 86400s
+ * 1s = 1000ms = 1e9ns
+ *
+ * 2^31 = 2.14e9
+ * 2^63 = 9.22e18
+ *
+ * 2^31d = 2.14e9d = 5.87e6y
+ * 2^31s = 2.48e4d = 68.0y
+ * 2^31ms = 24.8d
+ * 2^31ns = 2.14e3ms = 2.14s
+
+ * 2^63s = 1.06e14d = 2.92e11y
+ * 2^63ms = 9.22e15s = 1.06e11d = 2.92e8y
+ * 2^63us = 9.22e12s = 1.06e8d = 2.92e5y
+ * 2^63ns = 9.22e9s = 1.06e5d = 292y
+ */
+
+#include <assert.h>
+
+static const int32 MILLISECONDS_PER_SECOND = 1000;
+static const int32 MILLISECONDS_PER_MINUTE = 1000 * 60; // MILLISECONDS_PER_SECOND * 60;
+static const int32 MILLISECONDS_PER_HOUR = 1000 * 60 * 60; // MILLISECONDS_PER_MINUTE * 60;
+static const int32 MILLISECONDS_PER_DAY = 1000 * 60 * 60 * 24; // MILLISECONDS_PER_HOUR * 24;
+
+public
+boolean Ctime_breakMilliseconds(const int64 milliseconds,
+        int64 * outDays, int32 * outMillisecondOfDay) {
+    int32 i = (int32) (milliseconds % MILLISECONDS_PER_DAY);
+    if (i < 0) {
+        i += MILLISECONDS_PER_DAY;
+    }
+    if (outDays != NULL) {
+        *outDays = (milliseconds - i) / MILLISECONDS_PER_DAY;
+    }
+    if (outMillisecondOfDay != NULL) {
+        *outMillisecondOfDay = i;
+    }
+    return true;
+}
+
+public
+int64 Ctime_totalMilliseconds(int64 days, int32 millisecondOfDay) {
+    assert(millisecondOfDay >= 0 && millisecondOfDay < MILLISECONDS_PER_DAY);
+    return MILLISECONDS_PER_DAY * days + millisecondOfDay;
+}
+
+public
+boolean Ctime_breakMillisecondOfDay(const int32 millisecondOfDay,
+        int32 * outHour, int32 * outMinute, int32 * outSecond, int32 * outMillisecond) {
+    if (millisecondOfDay < 0 || millisecondOfDay > MILLISECONDS_PER_DAY) {
+        return false;
+    }
+
+    if (millisecondOfDay == MILLISECONDS_PER_DAY) {
+        // troublesome leap second
+        if (outHour != NULL) {
+            *outHour = 23;
+        }
+        if (outMinute != NULL) {
+            *outMinute = 59;
+        }
+        if (outSecond != NULL) {
+            *outSecond = 60;
+        }
+        if (outMillisecond != NULL) {
+            *outMillisecond = 0;
+        }
+    } else {
+        if (outHour != NULL) {
+            *outHour = (millisecondOfDay / MILLISECONDS_PER_HOUR) % 24;
+        }
+        if (outMinute != NULL) {
+            *outMinute = (millisecondOfDay / MILLISECONDS_PER_MINUTE) % 60;
+        }
+        if (outSecond != NULL) {
+            *outSecond = (millisecondOfDay / MILLISECONDS_PER_SECOND) % 60;
+        }
+        if (outMillisecond != NULL) {
+            *outMillisecond = millisecondOfDay % MILLISECONDS_PER_SECOND;
+        }
+    }
+    return true;
+}
+
+public
+int32 Ctime_toMillisecondOfDay(int32 hh, int32 mm, int32 ss, int32 sss) {
+    assert(hh >= 0 && hh < 60);
+    assert(mm >= 0 && mm < 60);
+    assert(ss >= 0 && ss < 60);
+    assert(sss >= 0 && sss < 1000);
+    return MILLISECONDS_PER_HOUR * hh + MILLISECONDS_PER_MINUTE * mm
+            + MILLISECONDS_PER_SECOND * ss + sss;
+}
+
+/*******************
  * Date on Gregorian calendar
  */
 
@@ -42,7 +140,7 @@ static const int32 Y100 = 36524; // Y4 * 25 - 1
 static const int32 Y400 = 146097; // Y100 * 4 + 1
 
 public
-boolean TimeUtil_breakDays(const int64 days,
+boolean Ctime_breakDays(const int64 days,
         int32 * outYear, int32 * outDayOfYear,
         int32 * outMonthOfYear, int32 * outDayOfMonth,
         int32 * outWeekOfYear, int32 * outDayOfWeek) {
@@ -117,7 +215,7 @@ boolean TimeUtil_breakDays(const int64 days,
 }
 
 public
-int64 TimeUtil_totalDays(const int32 year, const int32 dayOfYear) {
+int64 Ctime_totalDays(const int32 year, const int32 dayOfYear) {
     assert(dayOfYear >= 0 && dayOfYear < (isLeapYear(year) ? 366 : 355));
     // align to year 0 to avoid overflow
     int32 numLeapYears = year > 0
@@ -127,7 +225,7 @@ int64 TimeUtil_totalDays(const int32 year, const int32 dayOfYear) {
 }
 
 public
-int32 TimeUtil_toDayOfYear(const int32 year, const int32 monthOfYear, const int32 dayOfMonth) {
+int32 Ctime_toDayOfYear(const int32 year, const int32 monthOfYear, const int32 dayOfMonth) {
     assert(monthOfYear >= 0 && monthOfYear < 12);
 
     const int32 * daysOverMonths = getDaysOverMonthsByYear(year);
@@ -141,16 +239,16 @@ int32 TimeUtil_toDayOfYear(const int32 year, const int32 monthOfYear, const int3
 }
 
 public
-int TimeUtil_toUtcString(int64 timestamp, byte * dst) {
+int Ctime_toUtcString(int64 timestamp, byte * dst) {
     int64 days;
     int32 millisecondOfDay;
-    TimeUtil_breakMilliseconds(timestamp, &days, &millisecondOfDay);
+    Ctime_breakMilliseconds(timestamp, &days, &millisecondOfDay);
 
     int32 year, monthOfYear, dayOfMonth;
-    TimeUtil_breakDays(days, &year, NULL, &monthOfYear, &dayOfMonth, NULL, NULL);
+    Ctime_breakDays(days, &year, NULL, &monthOfYear, &dayOfMonth, NULL, NULL);
 
     int32 hh, mm, ss, sss;
-    TimeUtil_breakMillisecondOfDay(millisecondOfDay, &hh, &mm, &ss, &sss);
+    Ctime_breakMillisecondOfDay(millisecondOfDay, &hh, &mm, &ss, &sss);
 
     return sprintf(dst, "%04d-%02d-%02dT%02d:%02d:%02d.%03dZ",
             year, monthOfYear + 1, dayOfMonth + 1, hh, mm, ss, sss);
---
+++ src/ctype/Ctime.h
@@ -0,0 +1,33 @@
+#ifndef _INCLUDED_CTIME
+#define _INCLUDED_CTIME
+
+public
+boolean Ctime_breakMilliseconds(const int64 milliseconds,
+        int64 * outDays, int32 * outMillisecondOfDay);
+
+public
+int64 Ctime_totalMilliseconds(int64 days, int32 millisecondOfDay);
+
+public
+boolean Ctime_breakMillisecondOfDay(const int32 millisecondOfDay,
+        int32 * outHour, int32 * outMinute, int32 * outSecond, int32 * outMillisecond);
+
+public
+int32 Ctime_toMillisecondOfDay(int32 hh, int32 mm, int32 ss, int32 sss);
+
+public
+boolean Ctime_breakDays(const int64 days,
+        int32 * outYear, int32 * outDayOfYear,
+        int32 * outMonthOfYear, int32 * outDayOfMonth,
+        int32 * outWeekOfYear, int32 * outDayOfWeek);
+
+public
+int64 Ctime_totalDays(const int32 year, const int32 dayOfYear);
+
+public
+int32 Ctime_toDayOfYear(const int32 year, const int32 monthOfYear, const int32 dayOfMonth);
+
+public
+int Ctime_toUtcString(const long timestamp, byte * dst);
+
+#endif // _INCLUDED_CTIME
