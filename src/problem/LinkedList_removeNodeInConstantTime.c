5c7416ff904497ef93956a00251d676ca83cf8c6
tanghao <tanghao.hb@gmail.com>
2021-08-10 20:36:00 +0800

    add problem LinkedList_removeNodeInConstantTime

---
+++ src/problem/LinkedList_removeNodeInConstantTime.c
@@ -0,0 +1,12 @@
+/**
+ * remove given node from a list, in O(1) time
+ *
+ * 若为尾结点，则需从头遍历一次，worst O(n)
+ * 否则，将本结点数据与下一结点数据交换，然后删除下一结点，average O(1)
+ *
+ * 这个解法初看很巧妙；一旦有其它指针指向链表中的结点，则后果不可预料
+ * 不是正经的工程做法
+ */
+
+void LinkedList_removeNodeInConstantTime(LinkedListNode * head, LinkedListNode * target) {
+}
