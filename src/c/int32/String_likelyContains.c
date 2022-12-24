a23768e05a45d9e15bfab1f59102a4ffac8dc2ab
tanghao <tanghao.hb@gmail.com>
2022-12-24 23:03:54 +0800

    move String_likelyContains

--- src/c/int32/String_likelyContains.c
+++ src/c/int32/String_likelyContains.c
@@ -1,11 +1,13 @@
 #include <assert.h>
 
-static int onePassHashMod(int last, int ch, int seed, int m) {
+static
+int onePassHashMod(int last, int ch, int seed, int m) {
     return (last * seed + ch) % m;
 }
 
 // x ^ n % m
-static int powMod(int x, int n, int m) {
+static
+int powMod(int x, int n, int m) {
     assert(n >= 0);
     x = x % m;
     int result = 1;
@@ -23,32 +25,28 @@ static int powMod(int x, int n, int m) {
  * "likely" contains() via iterable hash function
  * return false for definitely not contains, true for much likely contains
  */
-int Subarray_likelyContains(byte * s, int sl, int sr, byte * p, int pl, int pr) {
-    s += sl;
-    int sn = sr - sl;
-    p += pl;
-    int pn = pr - pl;
-
+public
+int String_likelyContains(byte * s, int sLen, byte * p, int pLen) {
     const int seed = 233;
     const int m = 31;
 
     int pk = 0;
-    for (int i = 0; i < pn; i++) {
+    for (int i = 0; i < pLen; i++) {
         pk = onePassHashMod(pk, p[i], seed, m);
     }
 
-    int pm = powMod(seed, pn, m);
+    int pm = powMod(seed, pLen, m);
 
-    int ska[sn + 1];
+    int ska[sLen + 1];
     ska[0] = 0;
-    for (int i = 1; i <= sn; i++) {
+    for (int i = 1; i <= sLen; i++) {
         ska[i] = onePassHashMod(ska[i - 1], s[i], seed, m);
-        if (i < pn) {
+        if (i < pLen) {
             continue;
         }
-        int k = (s[i - pn] * pm + pk) % m;
+        int k = (s[i - pLen] * pm + pk) % m;
         if (k == s[i]) {
-            return i - pn;
+            return i - pLen;
         }
     }
     return -1;
